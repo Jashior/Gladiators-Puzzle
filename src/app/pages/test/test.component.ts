9613a7c30d15fe04c1693c9b2ccabea8e0ec75ff
saintjuniper <saintjuniper73@gmail.com>
2021-10-25 17:48:54 +0100

    split outcome in proof

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  styleUrls: ['./test.component.css'],
 })
 export class TestComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+let activeLink = null;
+
+window.addEventListener('scroll', () => {
+  $$('.section').forEach((section) => {
+    const { y } = section.getBoundingClientRect();
+
+    if (y <= 0) {
+      if (activeLink != null) {
+        activeLink.classList.remove('active');
+      }
+
+      activeLink = $(`.nav-link[href~="#${section.id}"]`);
+      activeLink.classList.add('active');
+    }
+  });
+});
